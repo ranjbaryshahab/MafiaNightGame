f332eeb65812cc432c63320559340839ee49a428
shahab-7596 <shahab.ranjbary.1996@gmail.com>
2019-11-29 17:02:14 +0330

    Citizen class body created.

--- src/ir/maktab/java32/projects/mafianightgame/model/human/citizen/Citizen.java
+++ src/ir/maktab/java32/projects/mafianightgame/model/human/citizen/Citizen.java
@@ -2,6 +2,30 @@ package ir.maktab.java32.projects.mafianightgame.model.human.citizen;
 
 import ir.maktab.java32.projects.mafianightgame.model.human.Human;
 
+import java.util.List;
+
 public class Citizen extends Human {
-    
+    private Integer idCitizen;
+    private List<Human> humanList;
+
+    public Integer getIdCitizen() {
+        return idCitizen;
+    }
+
+    public void setIdCitizen(Integer idCitizen) {
+        this.idCitizen = idCitizen;
+    }
+
+    public List<Human> getHumanList() {
+        return humanList;
+    }
+
+    public void setHumanList(List<Human> humanList) {
+        this.humanList = humanList;
+    }
+
+    @Override
+    public String vote() {
+        return super.vote();
+    }
 }
