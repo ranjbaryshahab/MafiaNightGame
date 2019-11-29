e8d0928117ba03830e3ad08a456606c7f9f423e3
shahab-7596 <shahab.ranjbary.1996@gmail.com>
2019-11-29 16:35:00 +0330

    Mafia class body created.

--- src/ir/maktab/java32/projects/mafianightgame/model/human/mafia/Mafia.java
+++ src/ir/maktab/java32/projects/mafianightgame/model/human/mafia/Mafia.java
@@ -1,6 +1,31 @@
 package ir.maktab.java32.projects.mafianightgame.model.human.mafia;
 
 import ir.maktab.java32.projects.mafianightgame.model.human.Human;
+import ir.maktab.java32.projects.mafianightgame.model.human.citizen.Citizen;
+
+import java.util.List;
 
 public class Mafia extends Human {
+    private Integer idMafia;
+    private List<Citizen> citizens;
+
+    public Integer getIdMafia() {
+        return idMafia;
+    }
+
+    public void setIdMafia(Integer idMafia) {
+        this.idMafia = idMafia;
+    }
+
+    public List<Citizen> getCitizens() {
+        return citizens;
+    }
+
+    public void setCitizens(List<Citizen> citizens) {
+        this.citizens = citizens;
+    }
+
+    public String voteByMafia(){
+        return null;
+    }
 }
