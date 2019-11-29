805ae64b3aa4d788c3b2c9b6ff6969ab7229c5e9
shahab-7596 <shahab.ranjbary.1996@gmail.com>
2019-11-29 14:48:12 +0330

    Abstract class human is create

--- src/ir/maktab/java32/projects/mafianightgame/model/human/Human.java
+++ src/ir/maktab/java32/projects/mafianightgame/model/human/Human.java
@@ -1,5 +1,40 @@
 package ir.maktab.java32.projects.mafianightgame.model.human;
 
 public abstract class Human {
+    private Integer id;
+    private String name;
+    private boolean talk;
+    private String message;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isTalk() {
+        return talk;
+    }
+
+    public void setTalk(boolean talk) {
+        this.talk = talk;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
