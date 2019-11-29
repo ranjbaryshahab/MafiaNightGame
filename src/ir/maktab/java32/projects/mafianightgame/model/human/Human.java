822492d35124fbc3adf455d6288237623dbbebdf
shahab-7596 <shahab.ranjbary.1996@gmail.com>
2019-11-29 15:55:44 +0330

    Talk property name change to canbeTalk then make new property which name is alive finally toString method is override.

--- src/ir/maktab/java32/projects/mafianightgame/model/human/Human.java
+++ src/ir/maktab/java32/projects/mafianightgame/model/human/Human.java
@@ -3,7 +3,8 @@ package ir.maktab.java32.projects.mafianightgame.model.human;
 public abstract class Human {
     private Integer id;
     private String name;
-    private boolean talk;
+    private boolean canBeTalk;
+    private boolean alive;
     private String message;
 
     public Integer getId() {
@@ -22,19 +23,38 @@ public abstract class Human {
         this.name = name;
     }
 
-    public boolean isTalk() {
-        return talk;
-    }
-
-    public void setTalk(boolean talk) {
-        this.talk = talk;
-    }
-
     public String getMessage() {
         return message;
     }
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public boolean isCanBeTalk() {
+        return canBeTalk;
+    }
+
+    public void setCanBeTalk(boolean canBeTalk) {
+        this.canBeTalk = canBeTalk;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", canBeTalk=" + canBeTalk +
+                ", alive=" + alive +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
