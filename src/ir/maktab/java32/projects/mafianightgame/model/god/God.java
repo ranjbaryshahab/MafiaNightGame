15c7bc654895d593b3d6e76d5a711be8bd0cf0e4
mammad <mh.r9976@gmail.com>
2019-11-29 14:40:25 +0330

    God class created

--- src/ir/maktab/java32/projects/mafianightgame/model/god/God.java
+++ src/ir/maktab/java32/projects/mafianightgame/model/god/God.java
@@ -1,4 +1,13 @@
 package ir.maktab.java32.projects.mafianightgame.model.god;
 
 public class God {
+    public static God god = null;
+    private God(){
+    }
+    public static God getGod(){
+        if(god == null){
+            god = new God();
+        }
+        return god;
+    }
 }
