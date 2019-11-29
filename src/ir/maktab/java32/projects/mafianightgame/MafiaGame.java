22066b392c5458fd2a59ce5849052616a7c0eebb
shahab-7596 <shahab.ranjbary.1996@gmail.com>
2019-11-29 15:09:30 +0330

    LanguageLevel is change to jdk_13

--- src/ir/maktab/java32/projects/mafianightgame/MafiaGame.java
+++ src/ir/maktab/java32/projects/mafianightgame/MafiaGame.java
@@ -1,10 +1,7 @@
 package ir.maktab.java32.projects.mafianightgame;
 
-import ir.maktab.java32.projects.mafianightgame.model.god.God;
-
 public class MafiaGame {
     public static void main(String[] args) {
-
 
     }
 }
