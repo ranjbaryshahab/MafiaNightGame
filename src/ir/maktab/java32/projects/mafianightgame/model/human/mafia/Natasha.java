61fd53f2c58bfa17e40e3a39b4e067324232614e
Ali <alikheiry1897@gmail.com>
2019-12-01 23:28:07 +0330

    Complete silent method of natasha class.

--- src/ir/maktab/java32/projects/mafianightgame/model/human/mafia/Natasha.java
+++ src/ir/maktab/java32/projects/mafianightgame/model/human/mafia/Natasha.java
@@ -2,6 +2,10 @@ package ir.maktab.java32.projects.mafianightgame.model.human.mafia;
 
 import ir.maktab.java32.projects.mafianightgame.model.human.citizen.Citizen;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Natasha extends Mafia {
     private static Natasha natasha = null;
 
@@ -16,6 +20,24 @@ public class Natasha extends Mafia {
     }
 
     //Get citizen most message length and canBeTalk = false
-    public void silent(Citizen citizen) {
+    public Citizen silent(List<Citizen> citizenList) {
+        //The list of length message of citizens
+        List<Integer> lengthMessageList = new ArrayList<>();
+        //Add length message of citizens in lengthMessageList.
+        for (Citizen citizen : citizenList)
+            lengthMessageList.add(citizen.getMessage().length());
+        //Find max length of lengthMessageList.
+        int maxLength = lengthMessageList.stream().max(Comparator.comparing(Integer::valueOf)).get();
+        //Create talkative citizen.
+        Citizen talkativeCitizen = null;
+        //Find talkative citizen.
+        for (Citizen citizen : citizenList)
+            if (citizen.getMessage().length() == maxLength) {
+                talkativeCitizen = citizen;
+                break;
+            }
+        //Set canBeTalk property Talkative citizen equal false.
+        talkativeCitizen.setCanBeTalk(false);
+        return talkativeCitizen;
     }
 }
